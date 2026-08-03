9af0f2f6c287074fd7aa9b99573cbd17ce6274c4
Paras Agarwal <parasdevprojects@gmail.com>
2026-08-04 04:44:59 +0530

    chore: bump version to 1.2.7.4 (versionCode 2037)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ plugins {
 // DO NOT remove the ABI offsets in the onVariants block!
 // baseVersionCode = 2027 on its own wouldn't clear the old 1002004 bug. 
 // It only works because of the per-ABI +1M/2M/3M offsets.
-val baseVersionCode = 2036
-val baseVersionName = "1.2.7.3"
+val baseVersionCode = 2037
+val baseVersionName = "1.2.7.4"
 
 android {
     namespace = "com.rebelroot.omni"
